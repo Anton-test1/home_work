9952a1b79949f097318a4896b1a5ea4d712f8df9
Anton-test1 <ggkach@bk.ru>
2025-04-15 02:11:08 +0300

    Update 2_hw.py

--- 2_hw.py
+++ 2_hw.py
@@ -1,1 +1,17 @@
+def task_1() -> None:
+    integer = 99
+    print("Тип integer: (type(integer))")
 
+    float = 7.62
+    print("Тип float: (type(float))")
+
+    string = "Привет"
+    print("Тип string: (type(string))")
+
+    list = [1, "три", 7.7, False]
+    print("Тип list: (type(list))")
+
+    boolean = False
+    print("Тип boolean: (type(boolean))")
+
+task_1()
