5fa44bc01b6e6e43cc045be032d45ffe6556c282
Anton-test1 <ggkach@bk.ru>
2025-04-15 03:17:10 +0300

    Update 2_hw.py

--- 2_hw.py
+++ 2_hw.py
@@ -15,3 +15,21 @@ def task_1() -> None:
     print("Тип boolean: (type(boolean))")
 
 task_1()
+
+#2#
+
+def task_2() -> list:
+    a = [1, 2, 3, 5, 8, 13, 21]
+    print(a[0:3])
+task_2()
+#Последовательность Фибоначчи#
+
+
+#3#
+
+
+def task_3(number: float) -> float:
+    return number ** 2
+#тип данных float, так как исходное число может быть не целое.
+A = task_3(5.7)
+print(f"Квадрат числа равен: {A}")
